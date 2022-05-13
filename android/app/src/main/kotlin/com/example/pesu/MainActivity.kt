cb04cf88f948a1c6067fa5b15c0fd8505f3eeed8
latika <latika.b@dikshatech.com>
2022-05-13 10:23:08 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pesu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pesu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/services/app_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class AppRouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    return data(settings);
+  }
+}
+
+data(RouteSettings settings) {
+  switch (settings.name) {
+  }
+  }
