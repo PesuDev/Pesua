ce59cc4b379092dfbb5041ac8a3bf3198edb60f1
Deepak Kumar Yadav <deepak.kryadav@dikshatech.com>
2022-05-26 01:04:28 +0530

    dashboard_module_enhancements

--- lib/utils/view/app_drawer_screen.dart
+++ lib/utils/view/app_drawer_screen.dart
@@ -9,70 +9,171 @@ class AppDrawerScreen extends StatefulWidget {
 }
 
 class _AppDrawerScreenState extends State<AppDrawerScreen> {
+  var _mainHeight;
+  var _mainWidth;
+
   @override
   Widget build(BuildContext context) {
+    _mainHeight = MediaQuery.of(context).size.height;
+    _mainWidth = MediaQuery.of(context).size.width;
+
     return Drawer(
       child: Container(
-        height: MediaQuery.of(context).size.height * 0.8,
-        width: MediaQuery.of(context).size.width * 0.3,
+        color: Colors.white,
+        height: _mainHeight * 0.8,
+        width: _mainWidth * 0.3,
         child: ListView(
           children: [
             Container(
-              child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(color: appThemeColor),
-                accountEmail: Text('deepak4@apple.com'),
-                accountName: Text('Deepak Kr'),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://media.springernature.com/full/springer-cms/rest/v1/img/18893370/v1/height/320'),
-                ),
+              height: _mainHeight * 0.035,
+              alignment: Alignment.center,
+              child: RichText(
+                text: TextSpan(
+                    text: 'PES',
+                    style: TextStyle(
+                        color: headingColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700),
+                    children: [
+                      TextSpan(
+                          text: ' University',
+                          style: TextStyle(color: appThemeContrastColor)),
+                    ]),
               ),
             ),
+            Divider(),
+            getDrawerDetails(context: context),
             getDrawerTiles(
                 context: context,
-                icon: Icon(
-                  Icons.assessment,
-                  color: appThemeColor,
-                  size: 20,
-                ),
+                icon: Icons.assessment,
                 title: 'My Courses',
                 callback: () {}),
+
             getDrawerTiles(
                 context: context,
-                icon: Icon(
-                  Icons.all_inclusive,
-                  color: appThemeColor,
-                  size: 20,
-                ),
+                icon: Icons.all_inclusive,
                 title: 'Examination Grievance Redressal System',
                 callback: () {}),
             getDrawerTiles(
                 context: context,
-                icon: Icon(
-                  Icons.addchart_sharp,
-                  color: appThemeColor,
-                  size: 20,
-                ),
+                icon: Icons.addchart_sharp,
                 title: 'Timetable',
                 callback: () {}),
             getDrawerTiles(
                 context: context,
-                icon: Icon(
-                  Icons.wysiwyg_outlined,
-                  color: appThemeColor,
-                  size: 20,
-                ),
+                icon: Icons.wysiwyg_outlined,
                 title: 'Attendance',
                 callback: () {}),
             getDrawerTiles(
                 context: context,
-                icon: Icon(
-                  Icons.weekend_outlined,
-                  color: appThemeColor,
-                  size: 20,
-                ),
+                icon: Icons.weekend_outlined,
                 title: 'Assignments',
-                callback: () {})
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'ISA Results',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'ESA Results',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Session Effectiveness',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Seating Info',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'ClassRoom Videos',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Backlog Registration',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Online Payments',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Placements',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Calender',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Announcements',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Notification',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Library',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'PES University',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'PESU Forums',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'CIE',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Bootstrap',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Transport',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'My Profile',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Setting',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Help',
+                callback: () {}),
+            getDrawerTiles(
+                context: context,
+                icon: Icons.weekend_outlined,
+                title: 'Logout',
+                callback: () {}),
           ],
         ),
       ),
@@ -81,26 +182,103 @@ class _AppDrawerScreenState extends State<AppDrawerScreen> {
 
   Widget getDrawerTiles(
       {required BuildContext context,
-      required Icon icon,
+      required IconData icon,
       required String title,
       required Function callback}) {
-    return ListTile(
+    return Container(
+      padding: EdgeInsets.only(
+        left: _mainWidth * 0.02,
+        right: _mainWidth * 0.02,
+        bottom: _mainHeight*0.01,
+          top:  _mainHeight*0.01,
+      ),
+
+      color: Colors.blue,
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 20,
+          ),
+          SizedBox(width: _mainWidth*0.02,),
+          Container(
+
+            width: _mainWidth*0.62,
+            child: Text(
+              title,
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500),
+            ),
+          )
+        ],
+      ),
+    );
+    /*ListTile(
+
       leading: Container(
           decoration: BoxDecoration(
               color: appThemeColor.withAlpha(50),
               borderRadius: BorderRadius.circular(5)),
           height: 30,
           width: 30,
-          child: icon),
+          child: Icon(
+            icon,
+            color: appThemeColor,
+            size: 20,
+          )),
       title: Text(
         title,
         style: TextStyle(
-          fontSize: 16,
-          color: Colors.black87,
-          fontWeight: FontWeight.w500
-        ),
+            fontSize: 16, color: Colors.black87, fontWeight: FontWeight.w500),
       ),
-      trailing: Icon(Icons.arrow_right,size: 15,),
+      trailing: Icon(
+        Icons.arrow_right,
+        size: 20,
+      ),
+    );*/
+  }
+
+  Widget getDrawerDetails({required BuildContext context}) {
+    return Container(
+      height: _mainHeight * 0.11,
+      color: Colors.white,
+      padding: EdgeInsets.only(left: _mainWidth * 0.02),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            radius: MediaQuery.of(context).size.height * 0.04,
+            backgroundImage: NetworkImage(
+                'https://media.springernature.com/full/springer-cms/rest/v1/img/18893370/v1/height/320'),
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+          Container(
+            width: _mainWidth * 0.5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Deepak Kr',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: Colors.black),
+                ),
+                Text('PESU ID : PES12345678', style: getTextStyle),
+                Text('SRN : PES12345678', style: getTextStyle),
+                Text('BRANCH : CSE', style: getTextStyle),
+                Text('Sem-7,Section A', style: getTextStyle),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  TextStyle get getTextStyle => TextStyle(
+      fontSize: 14, color: Colors.black87, fontWeight: FontWeight.w400);
 }
