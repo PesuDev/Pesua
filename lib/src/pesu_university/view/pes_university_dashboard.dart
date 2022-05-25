70e6c21fbc70d65423ee7d3e5c763f2415fa8368
latika <latika.b@dikshatech.com>
2022-05-25 11:30:08 +0530

    backlog ui

---
+++ lib/src/pesu_university/view/pes_university_dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PesUniversityDashboard extends StatefulWidget {
+  const PesUniversityDashboard({Key? key}) : super(key: key);
+
+  @override
+  State<PesUniversityDashboard> createState() => _PesUniversityDashboardState();
+}
+
+class _PesUniversityDashboardState extends State<PesUniversityDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
