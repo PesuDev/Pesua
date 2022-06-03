a8cf6690951586733907bcc85fba39b98d477365
latika <latika.b@dikshatech.com>
2022-06-03 19:33:03 +0530

    navigation

--- lib/utils/services/app_routes.dart
+++ lib/utils/services/app_routes.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pesu/src/attendance/view/attendance_dashboard.dart';
 import 'package:pesu/src/attendance/view/back_log_registration.dart';
+import 'package:pesu/src/help/view/help_dashboard.dart';
+import 'package:pesu/src/notification/view/notification.dart';
 import 'package:pesu/src/settings/view/settings.dart';
 import 'package:pesu/src/transport/view/transport_dashboard.dart';
 
@@ -25,6 +27,12 @@ data(RouteSettings settings) {
     case AppRoutes.backLog: return MaterialPageRoute(
         builder: (_) =>BackLogRegistration()
     );
+    case AppRoutes.help: return MaterialPageRoute(
+        builder: (_) =>HelpDashboard()
+    );
+    case AppRoutes.notification: return MaterialPageRoute(
+        builder: (_) =>NotificationDashboard()
+    );
   }
   }
 class AppRoutes {
@@ -34,5 +42,6 @@ class AppRoutes {
   static const String help="help";
   static const String backLog="backLog";
   static const String assignment="assignment";
+  static const String notification="notification";
 
 }
