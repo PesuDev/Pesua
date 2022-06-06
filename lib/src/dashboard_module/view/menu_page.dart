20fca5d316f19ba2d69e213607c83d6758364fc5
Deepak Kumar Yadav <deepak.kryadav@dikshatech.com>
2022-06-06 19:05:29 +0530

    ui_fixes

--- lib/src/dashboard_module/view/menu_page.dart
+++ lib/src/dashboard_module/view/menu_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pesu/src/dashboard_module/viewModel/dashboard_viewModel.dart';
+import 'package:pesu/utils/services/app_routes.dart';
 import 'package:pesu/utils/services/bottom_navigaton_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -59,17 +60,54 @@ class _MenuPageState extends State<MenuPage> {
           child: GridView.builder(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 4,
-                crossAxisSpacing: _mainHeight*0.01,
+                crossAxisSpacing: _mainHeight * 0.01,
                 childAspectRatio: 1,
                 mainAxisSpacing: _mainHeight * 0.05),
             itemBuilder: (_, index) {
               var data = _viewModel.getSettingsList()[index];
               return InkWell(
                 onTap: () {
-                  if(index == 0){
-                    // Navigate
-                  }else if (index==1){
-                    // Navigate
+                  if (index == 0) {
+                    //login
+
+                  } else if (index == 1) {
+                    //courses
+                  } else if (index == 2) {
+                    Navigator.pushNamed(context, AppRoutes.examination);
+                  } else if (index == 3) {
+                    Navigator.pushNamed(context, AppRoutes.timeTable);
+                  } else if (index == 4) {
+                    Navigator.pushNamed(context, AppRoutes.attendance);
+                  } else if (index == 5) {
+                    Navigator.pushNamed(context, AppRoutes.assignment);
+                  } else if (index == 6) {
+                    //ISA result
+                  } else if (index == 7) {
+                    Navigator.pushNamed(context, AppRoutes.seatingInfo);
+                  } else if (index == 8) {
+                    //session effectiveness
+                  } else if (index == 9) {
+                    //class room videos
+                  } else if (index == 10) {
+                    Navigator.pushNamed(context, AppRoutes.backLog);
+                  } else if (index == 11) {
+                    Navigator.pushNamed(context, AppRoutes.onlinePayments);
+                  } else if (index == 12) {
+                    Navigator.pushNamed(context, AppRoutes.placement);
+                  } else if (index == 13) {
+                    //calender
+                  } else if (index == 14) {
+                    //Announcements
+                  } else if (index == 15) {
+                    //Library
+                  } else if (index == 16) {
+                    Navigator.pushNamed(context, AppRoutes.transport);
+                  } else if (index == 17) {
+                    //PESU Forms
+                  } else if (index == 18) {
+                    //my profile
+                  } else if (index == 19) {
+                    Navigator.pushNamed(context, AppRoutes.esaresults);
                   }
                 },
                 child: Container(
@@ -81,10 +119,10 @@ class _MenuPageState extends State<MenuPage> {
                       Icon(
                         data.icon,
                         color: Colors.grey.shade400,
-                        size: _mainHeight*0.04,
+                        size: _mainHeight * 0.04,
                       ),
                       SizedBox(
-                        height: _mainHeight*0.01,
+                        height: _mainHeight * 0.01,
                       ),
                       Text(
                         data.name ?? '',
--- lib/src/dashboard_module/viewModel/dashboard_viewModel.dart
+++ lib/src/dashboard_module/viewModel/dashboard_viewModel.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pesu/src/dashboard_module/model/settings_model.dart';
+import 'package:pesu/utils/services/app_routes.dart';
 
 class DashboardViewModel extends ChangeNotifier{
 
   List<SettingsModel> getSettingsList(){
     return [
-      SettingsModel(name: 'Login',icon: Icons.settings,callback: (){}),
+      SettingsModel(name: 'Login',icon: Icons.settings,callback: (){},),
       SettingsModel(name: 'My Courses',icon: Icons.settings,callback: (){}),
       SettingsModel(name: 'Exam Grievances',icon: Icons.settings,callback: (){}),
       SettingsModel(name: 'Time Table',icon: Icons.settings,callback: (){}),
@@ -25,7 +26,7 @@ class DashboardViewModel extends ChangeNotifier{
       SettingsModel(name: 'Transport',icon: Icons.settings,callback: (){}),
       SettingsModel(name: 'PESU Forums',icon: Icons.settings,callback: (){}),
       SettingsModel(name: 'My Profile',icon: Icons.settings,callback: (){}),
-      SettingsModel(name: 'Library',icon: Icons.settings,callback: (){}),
+      SettingsModel(name: 'ESA Results',icon: Icons.settings,callback: (){}),
     ];
   }
 
