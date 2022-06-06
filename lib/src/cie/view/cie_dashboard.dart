33be17a4a4738fea9d1f4e0ac04e30e58c3e0bf1
latika <latika.b@dikshatech.com>
2022-06-06 11:56:52 +0530

    cie dashboard

---
+++ lib/src/cie/view/cie_dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CieDashboard extends StatefulWidget {
+  const CieDashboard({Key? key}) : super(key: key);
+
+  @override
+  State<CieDashboard> createState() => _CieDashboardState();
+}
+
+class _CieDashboardState extends State<CieDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
