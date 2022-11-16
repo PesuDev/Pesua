74c0f148d7ed0702787777a7ab88ff46b6418a49
latika <latika.b@dikshatech.com>
2022-11-16 12:24:37 +0530

    session effectiveness issue

--- lib/src/session_effectiveness/view/session_effectiveness.dart
+++ lib/src/session_effectiveness/view/session_effectiveness.dart
@@ -126,6 +126,7 @@ var todayDays;
 
 
 
+var selectedSessionTime="Please Select Session Time";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -194,10 +195,11 @@ var todayDays;
                                   .toList(),
                               onChanged: (item) {
                                 print("Oye");
-                                time=[];
+
+
                                 setState(() {
                                   subject=item;
-
+time=[];
                                for(var dataVal in data.sessionEffectivenessModel!.timetableList!){
                                     if(dataVal.day==todayDays &&dataVal.subjectName==item){
 print("data ${dataVal}");
@@ -295,39 +297,22 @@ print("data ${dataVal}");
                             fontWeight: FontWeight.w700,fontSize: 15,
                         ),),
                         SizedBox(height: 10,),
-                    time.length>0?    Container(
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey)
+                    time.length>0?    InkWell(
+                      onTap: (){
+                        _timeBottomSheet(time: time);
+                      },
+                      child: Container(
+                        width: double.infinity,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey)
+                            ),
+                            height: 34,
+                            child:Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("$selectedSessionTime "),
+                            ),
                           ),
-                          height: 34,
-                          child: DropdownButtonFormField<String>(
-                              decoration: InputDecoration.collapsed(hintText: ''),
-
-                              hint: Padding(
-                                padding: const EdgeInsets.only(top: 5, left: 10),
-                                child:
-
-                                Text("data")
-                              ),
-
-                              items:
-                            time.map((droptime) => DropdownMenuItem<String>(
-                              value: droptime,
-                              child:
-
-                              Text(
-                              "${droptime.toString()}"
-                              ),
-                            )
-                              ,).toList(),
-                              onChanged: (item) {
-                                // _timeBottomSheet();
-
-                              }
-                              ,icon: Icon(Icons.keyboard_arrow_down),
-                            iconSize: 15,
-                              ),
-                        ):Container(),
+                    ):Container(),
                         data.sessionEffectivenessModel!.timetableList!=null && time.toString().isNotEmpty ?
                        Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -622,7 +607,7 @@ print("data ${dataVal}");
 
   }
 
-  void _timeBottomSheet() {
+  void _timeBottomSheet({required List<String> time}) {
 
     showModalBottomSheet(
         context: context,
@@ -630,41 +615,25 @@ print("data ${dataVal}");
           return
             new Container(
              // color: Colors.black,
-              child: ListView(
-                children: [
-                  ListTile(
-                    leading: Radio(
-                        value: 8,
-                        groupValue: timeRadio,
-                        onChanged: (int? val){
-                          setState(() {
-                            timeRadio=val;
+              child: ListView.builder
 
+                (
+                itemCount: time.length,
+                itemBuilder: (BuildContext context, int index) {
+                return InkWell(
 
-                          });
-                          print(timeRadio);
-                          // print(val);
+                    onTap: (){
+                      setState(() {
+                        selectedSessionTime=time[index];
+                        Navigator.pop(context);
+                      });
+                    },
+                    child: Container(
 
-                        }),
-                    title: Text('Time',style: TextStyle(color: Colors.black),),
-                  ),
-                  ListTile(
-                    leading: Radio(
-                        value: 9,
-                        groupValue: timeRadio,
-                        onChanged: (int? val){
-                          setState(() {
-                            timeRadio=val;
+                      margin: EdgeInsets.only(left: 15,right: 15,top: 20,bottom: 20),
+                      child: Text("${time[index]}"),));
+              },
 
-
-                          });
-                          print(timeRadio);
-                          // print(val);
-
-                        }),
-                    title: Text('Time',style: TextStyle(color: Colors.black),),
-                  ),
-                ],
               ),
             );
         });
